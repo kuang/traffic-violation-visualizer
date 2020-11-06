19c536fe2bf6572181e6b95f86f20f71cfb9fce0
Justin Kuang <zk65@cornell.edu>
2020-11-05 23:09:26 -0500

    2020 4 year update. release notes- fixed bug with accident attribute, redesigned logic to be more scalable, added DUI attribute, adds driver's home and car info to each popup

--- 911.js
+++ 911.js
@@ -1,162 +1,128 @@
-var map, src = "https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?date_of_stop=";
-
-var markersArray = [];
-var attributesChecked = {
-    "accident": {
-        "Yes": false,
-        "No": false
-    },
-    "gender": {
-        "M": false,
-        "F": false
-    },
-    "fatal": {
-        "Yes": false,
-        "No": false
-    },
-    "race": { // Yes, why not feed the racists Yiyi
-        "WHITE": false,
-        "ASIAN": false,
-        "BLACK": false,
-        "OTHER": false
-    },
-    "violation_type": {
-        "Citation": false,
-        "Warning": false
-    }
+let map, src = "https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?date_of_stop=";
+let openInfoWindow;
+let markersArray = [];
+let attributesChecked = {
+    // bool
+    "dui": null,
+    "accident": null,
+    // M, F
+    "gender": null,
+    // bool
+    "fatal": null,
+    // WHITE", "ASIAN", "BLACK", "OTHER", "None"
+    // Yes, why not feed the racists Yiyi
+    "race": null,
+    // Citation, Warning
+    "violation_type": null
 };
 
-function resetAttribute(attribute) {
-    if (attribute != "all") {
-        // Reset selected attribute
-        for (var setting in attributesChecked[attribute]) {
-            if (attributesChecked[attribute].hasOwnProperty(setting)) {
-                attributesChecked[attribute][setting] = false;
-            }
-        }
-    } else {
-        // Reset all attributes
-        for (var attr in attributesChecked) {
-            if (attributesChecked.hasOwnProperty(attr)) {
-                for (var setting in attributesChecked[attr]) {
-                    if (attributesChecked[attr].hasOwnProperty(setting)) {
-                        attributesChecked[attr][setting] = false;
-                    }
-                }
-            }
-        }
+function resetAttributes() {
+    document.getElementById("accidentNo").checked = false;
+    document.getElementById("accidentYes").checked = false;
+    document.getElementById("duiNo").checked = false;
+    document.getElementById("duiYes").checked = false;
+    document.getElementById("genderMale").checked = false;
+    document.getElementById("genderFemale").checked = false;
+    document.getElementById("fatalYes").checked = false;
+    document.getElementById("fatalNo").checked = false;
+    document.getElementById("raceWhite").checked = false;
+    document.getElementById("raceBlack").checked = false;
+    document.getElementById("raceAsian").checked = false;
+    document.getElementById("raceOther").checked = false;
+    document.getElementById("vioWarning").checked = false;
+    document.getElementById("vioCitation").checked = false;
+
+    // Reset all attributes
+    for (let attr in attributesChecked) {
+        attributesChecked[attr] = null;
     }
+
 }
 
 
 function setOnMap() {
-    for (var i = 0; i < markersArray.length; i++) {
-        var check = true;
-        var tempMarker = markersArray[i];
-        for (var attr in attributesChecked) {
-            if (attributesChecked.hasOwnProperty(attr)) {
-                var attributeChoices = attributesChecked[attr];
-                for (var setting in attributeChoices) {
-                    if (attributeChoices.hasOwnProperty(setting)) {
-                        if (attributeChoices[setting] && tempMarker[attr] != setting) {
-                            check = false;
-                        }
-                    }
-                }
+    markersArray.forEach(marker => {
+        let check = true;
+        for (let attr in attributesChecked) {
+            if (attributesChecked[attr] != null && attributesChecked[attr] != marker[attr]) {
+                check = false;
             }
         }
         if (check) {
-            markersArray[i].setMap(map);
+            marker.setMap(map);
         } else {
-            markersArray[i].setMap(null);
+            marker.setMap(null);
         }
-    }
+    });
 }
 
 function initListeners() {
-    document.getElementById("accidentYes").addEventListener("click", function() {
-        resetAttribute("accident");
-        attributesChecked.accident.Yes = true;
+    document.getElementById("accidentYes").addEventListener("click", function () {
+        attributesChecked.accident = true;
         setOnMap();
     });
-    document.getElementById("accidentNo").addEventListener("click", function() {
-        resetAttribute("accident");
-        attributesChecked.accident.No = true;
+    document.getElementById("accidentNo").addEventListener("click", function () {
+        attributesChecked.accident = false;
         setOnMap();
     });
-    document.getElementById("genderMale").addEventListener("click", function() {
-        resetAttribute("gender");
-        attributesChecked.gender.M = true;
+    document.getElementById("duiYes").addEventListener("click", function () {
+        attributesChecked.dui = true;
         setOnMap();
     });
-    document.getElementById("genderFemale").addEventListener("click", function() {
-        resetAttribute("gender");
-        attributesChecked.gender.F = true;
+    document.getElementById("duiNo").addEventListener("click", function () {
+        attributesChecked.dui = false;
         setOnMap();
     });
-    document.getElementById("fatalYes").addEventListener("click", function() {
-        resetAttribute("fatal");
-        attributesChecked.fatal.Yes = true;
+    document.getElementById("genderMale").addEventListener("click", function () {
+        attributesChecked.gender = 'M';
         setOnMap();
     });
-    document.getElementById("fatalNo").addEventListener("click", function() {
-        resetAttribute("fatal");
-        attributesChecked.fatal.No = true;
+    document.getElementById("genderFemale").addEventListener("click", function () {
+        attributesChecked.gender = 'F';
         setOnMap();
     });
-    document.getElementById("raceWhite").addEventListener("click", function() {
-        resetAttribute("race");
-        attributesChecked.race.WHITE = true;
+    document.getElementById("fatalYes").addEventListener("click", function () {
+        attributesChecked.fatal = true;
         setOnMap();
     });
-    document.getElementById("raceAsian").addEventListener("click", function() {
-        resetAttribute("race");
-        attributesChecked.race.ASIAN = true;
+    document.getElementById("fatalNo").addEventListener("click", function () {
+        attributesChecked.fatal = false;
         setOnMap();
     });
-    document.getElementById("raceBlack").addEventListener("click", function() {
-        resetAttribute("race");
-        attributesChecked.race.BLACK = true;
+    document.getElementById("raceWhite").addEventListener("click", function () {
+        attributesChecked.race = "WHITE";
         setOnMap();
     });
-    document.getElementById("raceOther").addEventListener("click", function() {
-        resetAttribute("race");
-        attributesChecked.race.OTHER = true;
+    document.getElementById("raceAsian").addEventListener("click", function () {
+        attributesChecked.race = "ASIAN";
         setOnMap();
     });
-    document.getElementById("vioCitation").addEventListener("click", function() {
-        resetAttribute("violation_type");
-        attributesChecked.violation_type.Citation = true;
+    document.getElementById("raceBlack").addEventListener("click", function () {
+        attributesChecked.race = "BLACK";
         setOnMap();
     });
-    document.getElementById("vioWarning").addEventListener("click", function() {
-        resetAttribute("violation_type");
-        attributesChecked.violation_type.Warning = true;
+    document.getElementById("raceOther").addEventListener("click", function () {
+        attributesChecked.race = "OTHER";
         setOnMap();
     });
-    document.getElementById("showAll").addEventListener("click", function() {
-        document.getElementById("accidentNo").checked = false;
-        document.getElementById("accidentYes").checked = false;
-        document.getElementById("genderMale").checked = false;
-        document.getElementById("genderFemale").checked = false;
-        document.getElementById("fatalYes").checked = false;
-        document.getElementById("fatalNo").checked = false;
-        document.getElementById("raceWhite").checked = false;
-        document.getElementById("raceBlack").checked = false;
-        document.getElementById("raceAsian").checked = false;
-        document.getElementById("raceOther").checked = false;
-        document.getElementById("vioWarning").checked = false;
-        document.getElementById("vioCitation").checked = false;
-
-        resetAttribute("all");
+    document.getElementById("vioCitation").addEventListener("click", function () {
+        attributesChecked.violation_type = "Citation";
+        setOnMap();
+    });
+    document.getElementById("vioWarning").addEventListener("click", function () {
+        attributesChecked.violation_type = "Warning";
+        setOnMap();
+    });
+    document.getElementById("showAll").addEventListener("click", function () {
+        resetAttributes();
         setOnMap();
     });
     grabData();
 }
 
 function initMap() {
-    var myLatlng = new google.maps.LatLng(39.1547, -77.2405);
-    var mapOptions = {
+    let myLatlng = new google.maps.LatLng(39.1547, -77.2405);
+    let mapOptions = {
         zoom: 11,
         center: myLatlng
     }
@@ -169,7 +135,7 @@ function getEasternTime() {
 }
 
 function grabData() {
-    var date = getEasternTime(); // CAUTION: returns moment.js object, not native Date()
+    let date = getEasternTime(); // CAUTION: returns moment.js object, not native Date()
     // Data from yesterday only available if it's after 10 AM
     if (date.hours() >= 10) {
         date.subtract(1, "day");
@@ -182,35 +148,45 @@ function grabData() {
 }
 
 function getTrafficData(src) {
-    $.getJSON(src, function(data) {
-        for (var key = 0; key < data.length; key++) {
-            var description = "<p>" + data[key].description + "</p>";
-            var infowindow = new google.maps.InfoWindow({
+    $.getJSON(src, function (data) {
+        data.forEach(dp => {
+            let description = "<p>"
+                + dp.description
+                + "</br></br> This driver is from: "
+                + dp.driver_city + " "
+                + dp.driver_state
+                + "</br></br>car: "
+                + dp.color + " " + dp.make + " " + dp.model
+                + "</p> ";
+            let infowindow = new google.maps.InfoWindow({
                 content: description
             });
-            var lat = data[key].latitude;
-            var long = data[key].longitude;
+            let lat = dp.latitude;
+            let long = dp.longitude;
             if (lat && long) {
-                var myLatln = new google.maps.LatLng(lat, long);
-                var marker = new google.maps.Marker({
+                let myLatln = new google.maps.LatLng(lat, long);
+                let marker = new google.maps.Marker({
                     position: myLatln,
                     title: "",
                     info: description
                 });
-                marker.accident = data[key].contributed_to_accident;
-                marker.gender = data[key].gender;
-                marker.fatal = data[key].fatal;
-                marker.race = data[key].race;
-                marker.violation_type = data[key].violation_type;
+                marker.accident = dp.contributed_to_accident == "True";
+                marker.gender = dp.gender;
+                marker.fatal = dp.fatal == "Yes";
+                marker.race = dp.race;
+                marker.violation_type = dp.violation_type;
+                marker.dui = dp.alcohol == "Yes";
 
                 markersArray.push(marker);
 
                 marker.setMap(map);
-                google.maps.event.addListener(marker, 'click', function() {
+                google.maps.event.addListener(marker, 'click', function () {
                     infowindow.setContent(this.info);
                     infowindow.open(map, this);
+                    if(openInfoWindow) openInfoWindow.close();
+                    openInfoWindow = infowindow;
                 });
             }
-        }
+        });
     });
 }
